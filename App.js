006b9cd6c80d75057e250e6ea546e115b0133151
Lilly <lillyelectronics@gmail.com>
2025-04-04 22:21:54 -0600

    Delete node_modules folder and package-lock.json:

---
+++ App.js
@@ -0,0 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import TopicSelect from './screens/TopicSelect'; // You'll create this in Step 4
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="TopicSelect" component={TopicSelect} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
